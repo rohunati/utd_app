fc69912de4f3f2f3a2771daa74534c50a6a3ff92
Rohun Ati <ratijr@yahoo.com>
2013-10-05 11:44:26 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UtdApp::Application.config.secret_key_base = '208a40e06e9161fd9ab5828153df4192b59c0b82e6ebea6eb21dbe4c5f4a1119dd5932208e6b5e2f481810ae7ac0cf740328b077b4b8c446845d9a39fd9fdc62'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UtdApp::Application.config.session_store :cookie_store, key: '_utd_app_session'
